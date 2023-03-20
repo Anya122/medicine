64b7d88532e4f89de4f563f3a1b78e0ea518908c
Snegopad-izh <snegopad-izh@rambler.ru>
2023-03-20 22:20:47 +0400

    feat: add real time marker line in table

--- src/components/Main/Schedule/Calendar/Table/Table.tsx
+++ src/components/Main/Schedule/Calendar/Table/Table.tsx
@@ -1,13 +1,9 @@
-import React, {useEffect} from 'react';
+import React, {useEffect, useState} from 'react';
 import s from "./Table.module.scss"
 import {useAppSelector} from "../../../../../hooks/redux";
 
 const Table = () => {
     const visits = useAppSelector(state => state.visitsReducer);
-
-    useEffect(() => {
-        console.log(visits);
-    }, []);
 
     const timeMarkers = [
         "8.00",
@@ -20,8 +16,50 @@ const Table = () => {
         "15.00",
         "16.00",
         "17.00",
-    ]
+        "18.00",
+        "19.00",
+        "20.00",
+        "21.00",
+        "22.00",
+        "23.00",
+        "24.00",
+    ];
     const vLines = Array(6).fill(0).map((t, i) => i + 1);
+
+    // Смещение на 1 час - 81px
+
+    const [h, setH] = useState(() => {
+        const clock = new Date;
+        return clock.getHours();
+    });
+    const [m, setM] = useState(() => {
+        const clock = new Date;
+        return clock.getMinutes();
+    });
+    const [verticalShift, setVerticalShift] = useState(() => {
+        const res = ((h * 60 + m - 8 * 60) * 729) / (9 * 60);
+        return `${res.toString()}px`;
+    });
+
+    const getDate = () => {
+        const clock = new Date;
+        setH(clock.getHours());
+        setM(clock.getMinutes());
+    }
+
+    const calculateShift = () => {
+       return ((h * 60 + m - 8 * 60) * 729) / (9 * 60);
+    }
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+            getDate();
+            setVerticalShift(`${calculateShift().toString()}px`);
+            console.log(verticalShift);
+        }, 10000);
+        return () => clearInterval(interval);
+    }, [verticalShift]);
+
 
     return (
         <div className={s.table}>
@@ -30,12 +68,18 @@ const Table = () => {
                 {timeMarkers.map(t =>
                     <div key={t} className={s.hBlock}>
                         <div className={s.timeStamp}>{t}</div>
-                        <span className={s.hTimeLine}></span>
+                        <span className={s.hTimeLine}/>
                     </div>)}
             </div>
 
             <div className={s.vLines}>
-                {vLines.map(t => <span key={t} className={s.vBlock}></span>)}
+                {vLines.map(t => <span key={t} className={s.vBlock}/>)}
+            </div>
+
+            <div className={s.realTimeLine} style={{marginTop: verticalShift}}>
+                <div className={s.realTime}>{`${h}.${m}`}</div>
+                <span className={s.rtlCircle}/>
+                <span className={s.rtLine}/>
             </div>
 
 
