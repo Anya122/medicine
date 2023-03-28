ecca655f7ee3185acd76e4895ac5e95301128c15
Snegopad-izh <snegopad-izh@rambler.ru>
2023-03-28 19:34:07 +0400

    feat: animate time selector in table

--- src/components/Main/Schedule/Calendar/TimeSelector/TimeSelector.tsx
+++ src/components/Main/Schedule/Calendar/TimeSelector/TimeSelector.tsx
@@ -1,22 +1,60 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import s from "./TimeSelector.module.scss"
 
+import moment from 'moment';
+import 'moment/locale/ru';
 
 const TimeSelector = () => {
-    let selectedPeriod = {
-        startDay: 5,
-        endDay: 11,
-        month: "сентября",
-    }
 
+    moment.locale('ru');
+
+    const [time, setTime] = useState(moment());
+    const [selectedPeriod, setSelectedPeriod] = useState(
+        {
+            startDay: moment().weekday(0).format('D'),
+            endDay: moment().weekday(6).format('D'),
+            startWeekMonth: moment().weekday(0).format('MMMM'),
+            endWeekMonth: moment().weekday(6).format('MMMM'),
+        });
+
+    const changePeriod = (shift: number) => {
+        switch (shift) {
+            case 1:
+                setTime(moment(time).subtract(-1, 'week'));
+                break;
+            case 0:
+                setTime(moment(time).subtract(0, 'week'));
+                break;
+            case (-1):
+                setTime(moment(time).subtract(1, 'week'));
+                break;
+        }
+    };
+
+    useEffect(()=> {
+        setSelectedPeriod(
+        {
+            startDay: moment(time).weekday(0).format('D'),
+            endDay: moment(time).weekday(6).format('D'),
+            startWeekMonth: moment(time).weekday(0).format('MMMM'),
+            endWeekMonth: moment(time).weekday(6).format('MMMM'),
+        });
+    }, [time])
 
 
     return (
         <div className={s.timeSelector}>
-            <button className={s.leftButton}/>
+            <button className={s.leftButton} onClick={()=> {changePeriod(-1)}}/>
             <span className={s.period}>
-                {`${selectedPeriod.startDay} - ${selectedPeriod.endDay} ${selectedPeriod.month}`}</span>
-            <button className={s.rightButton}/>
+                {
+                    selectedPeriod.endWeekMonth === selectedPeriod.startWeekMonth ?
+                        `${selectedPeriod.startDay} - ${selectedPeriod.endDay} ${selectedPeriod.endWeekMonth}`
+                        :
+                        `${selectedPeriod.startDay} ${selectedPeriod.startWeekMonth}
+                        - ${selectedPeriod.endDay} ${selectedPeriod.endWeekMonth}`
+                }
+            </span>
+            <button className={s.rightButton} onClick={()=> {changePeriod(1)}}/>
         </div>
     );
 };
