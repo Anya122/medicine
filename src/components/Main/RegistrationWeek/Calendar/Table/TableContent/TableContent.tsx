8f350db886efa463e6f332300891efa9bfa725b6
Snegopad-izh <snegopad-izh@rambler.ru>
2023-04-04 22:22:10 +0400

    feat: horisontal shift for table content cards

--- src/components/Main/RegistrationWeek/Calendar/Table/TableContent/TableContent.tsx
+++ src/components/Main/RegistrationWeek/Calendar/Table/TableContent/TableContent.tsx
@@ -6,25 +6,24 @@ import moment from 'moment';
 const TableContent = () => {
 
 
-    const oneCard = (day?: string, startTime?: string, endTime?: string, content?: string) => {
+    const oneCard = (day: number, startTime: string, endTime: string, content: string) => {
 
         const stH = +(moment(startTime, 'LT').format('H'));
         const stM = +(moment(startTime, 'LT').format('m'));
         const eH = +(moment(endTime, 'LT').format('H'));
         const eM = +(moment(endTime, 'LT').format('m'));
-
         const duration = (eH * 60 + eM) - (stH * 60 + stM);
 
         const verticalShift = ((((stH * 60 + stM - 8 * 60) * 730) / (9 * 60)).toString() + 'px');
+        const horisontalShift = ((day-1) * 181).toString()+'px';
         const cardHeight = ((80 / 60) * duration).toString() + 'px';
         const contentFontSize = (((80 / 60) * duration)/2.3).toString() + 'px';
-
 
         const styles =
             {
                 backgroundColor: 'rgba(201, 242, 255, 1)',
                 fontSize: contentFontSize,
-                marginTop: verticalShift,
+                transform: `translateY(${verticalShift}) translateX(${horisontalShift})`,
                 height: cardHeight,
                 lineHeight: cardHeight
             }
@@ -41,10 +40,16 @@ const TableContent = () => {
         <div className={s.tableContent}>
 
             {
-                oneCard(undefined, "8:15", "8:35", "Петров")
+                oneCard(1, "8:15", "8:35", "Петров")
             }
             {
-                oneCard(undefined, "10:15", "10:59", "Сидоров")
+                oneCard(3, "12:08", "13:00", "Сидоров")
+            }
+            {
+                oneCard(4, "9:00", "10:00", "Яковлев")
+            }
+            {
+                oneCard(7, "10:15", "10:59", "Потапов")
             }
 
         </div>
